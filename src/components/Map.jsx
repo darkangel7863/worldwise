30793bbeb11108c966316c9137d8bc349b627946
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2025-04-25 18:14:15 +0300

    Handle the form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -12,17 +12,17 @@ import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
 import { useGeolocation } from '../hooks/useGeolocation';
 import Button from './Button';
+import { useURLPosition } from '../hooks/useURLPosition';
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
+
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  const [mapLat, mapLng] = useURLPosition();
   useEffect(
     function () {
       if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
